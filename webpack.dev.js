affe4e24ec23b4d054d9a9dd6ea863d34eb6051e
gwozniak <gwozniak@cyberforce.it>
2022-06-27 12:09:45 +0100

    setting up webpack serve

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,24 +5,42 @@ const {WebpackPluginServe: Serve} = require('webpack-plugin-serve')
 module.exports = () => {
     const plugins = []
 
+    const build = path.resolve(__dirname, 'build')
+
     plugins.push(
         new HtmlWebpackPlugin({
             hash: true,
             template: path.resolve(__dirname, 'src', 'index.template.html'),
-            entry: path.resolve(__dirname, 'build', 'index.html')
+            entry: path.resolve(build, 'index.html')
         })
     )
+
+    plugins.push(new Serve({
+        port: 3000,
+        static: build,
+        publicPath: build,
+        waitForBuild: true,
+        open: false,
+        historyFallback: true,
+        host: 'localhost',
+        liveReload: true,
+        hmr: true
+     }))
 
     return {
         mode: 'development', // production
         entry: [
+            'webpack-plugin-serve/client',
             path.resolve(__dirname, 'src', 'app.tsx')
         ],
         output: {
             publicPath: '',
-            path: path.resolve(__dirname, 'build'),
+            path: build,
             filename: 'app.js'
         },
+        watch: true,
+        devtool: 'inline-source-map',
+        stats: 'errors-only',
         module: {
             strictExportPresence: true,
             rules: [
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const {WebpackPluginServe: Serve} = require('webpack-plugin-serve')
 
 module.exports = () => {
     const plugins = []
